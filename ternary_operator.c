679e1d3eb1bbc75b3e8440856f4e97cda0515a8a
Bernard Mawulorm Kofi Wodoame <98283377+wodoame@users.noreply.github.com>
2023-02-04 16:08:22 -0800

    Create ternary_operator.c

---
+++ ternary_operator.c
@@ -0,0 +1,20 @@
+/* The ternary operator in C is a shorthand for an `if-else` statement.
+It has the following syntax: 
+
+condition ? expression1 : expression2;
+
+The `condition` is evaluated, and if it is true, `expression1` is executed. 
+If `condition` is false, `expression2` is executed instead.
+
+The value of the ternary operator is the value of whichever expression was executed. 
+It can be used to assign values to variables or to return values from functions. 
+
+Example: 
+
+*/
+
+int x = (a > b) ? a : b; 
+
+// In this example, the value of `a` is assigned to `x` if `a` is greater than `b`, 
+// and the value of `b` is assigned to `x` if `b` is greater than or equal to `a`. 
+
