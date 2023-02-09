d8d7436dfe8edde5af9c540200f9431867bb4099
Bernard Mawulorm Kofi Wodoame <98283377+wodoame@users.noreply.github.com>
2023-02-09 11:46:29 +0000

    Create arrays.c

---
+++ arrays.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5];  // declare an array of size 5
+
+    // populate the array with values
+    arr[0] = 10;
+    arr[1] = 20;
+    arr[2] = 30;
+    arr[3] = 40;
+    arr[4] = 50;
+
+    // print the values of the array
+    for (int i = 0; i < 5; i++) {
+        printf("arr[%d] = %d\n", i, arr[i]);
+    }
+
+    return 0;
+}
